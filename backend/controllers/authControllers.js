8e78b90d1e5cf10172081598eacf33bfe2d7195b
Juan Jesus <juanj.cesar@gmail.com>
2023-04-04 11:52:16 +0200

    backend-frontend de la autentificación unido

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -130,7 +130,9 @@ const revalidarToken = async (req, res = response) => {
 
     res.json({
         ok: true,
-        token
+        uid,
+        name,
+        token,
     })
 }
 
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -2,22 +2,43 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { LoginPage } from '../auth';
 import { CalendarPage } from '../calendar';
+import { useAuthStore } from '../hooks';
+import { useEffect } from 'react';
 
 
 export const AppRouter = () => {
 
-    const authStatus = 'not-authenticated'; // 'authenticated'; // 'not-authenticated';
+    const { status, checkAuthToken } = useAuthStore(); // apuntes junto a todo lo que sea JWT en frontend y agreagrlo aapuntes jwt
+    // const authStatus = 'not-authenticated'; // 'authenticated'; // 'not-authenticated';
 
+    useEffect(() => {
+        checkAuthToken();
+    }, [])
+
+    if (status === 'checking') {
+        return (
+            <h3>Cargando...</h3>
+        )
+    }
+   
 
     return (
         <Routes>
             {
-                ( authStatus === 'authenticated')  
-                    ? <Route path="/auth/*" element={ <LoginPage /> } />
-                    : <Route path="/*" element={ <CalendarPage /> } />
+                ( status === 'not-authenticated')  
+                    ? (
+                        <>
+                            <Route path="/auth/*" element={ <LoginPage /> } />
+                            <Route path="/*" element={ <Navigate to="/auth/login" /> } />
+                        </>
+                    )
+                    : (
+                        <>
+                            <Route path="/" element={ <CalendarPage /> } />
+                            <Route path="/*" element={ <Navigate to="/" /> } />
+                        </>
+                    )
             }
-
-            <Route path="/*" element={ <Navigate to="/auth/login" /> } />
         </Routes>
     )
 }
